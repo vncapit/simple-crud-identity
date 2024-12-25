1dd22a03df258a52ffd12a68a0c02d1aabb30e3d
vncapit <vncapit@gmail.com>
2024-12-25 18:26:08 +0700

    Request validation in spring

--- src/main/java/com/cap/identity_service/controller/UserController.java
+++ src/main/java/com/cap/identity_service/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.cap.identity_service.controller;
 
+import com.cap.identity_service.dto.request.ApiResponse;
 import com.cap.identity_service.dto.request.UserCreationRequest;
 import com.cap.identity_service.dto.request.UserUpdateRequest;
 import com.cap.identity_service.entity.User;
 import com.cap.identity_service.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +18,10 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    User createUser(@RequestBody UserCreationRequest request){
-        return userService.createUser(request);
+    ApiResponse<User> createUser(@RequestBody @Valid UserCreationRequest request){
+        ApiResponse<User> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.createUser(request));
+        return apiResponse;
     }
 
     @GetMapping
--- src/main/java/com/cap/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/cap/identity_service/dto/request/UserCreationRequest.java
@@ -1,9 +1,14 @@
 package com.cap.identity_service.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class UserCreationRequest {
+    @Size(min = 6, message = "INVALID_USERNAME")
     private String username;
+
+    @Size(min = 8, message = "INVALID_PASSWORD")
     private String password;
     private String firstName;
     private String lastName;
